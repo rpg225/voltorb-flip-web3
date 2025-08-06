8463d735e99d8f67a16b3a87ad15ce83b62cbe94
Rambod Goshtasbi <rpg225@gmail.com>
2025-08-06 19:07:32 -0400

    feat: created page, layout, and walletConnect

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,38 @@
 // frontend/src/app/page.js
+
+'use client';
+
+import { useState } from "react";
+import WalletConnect from '@/components/WalletConnect'
+
+
 export default function Home() {
+
+  const [currentAccount, setCurrentAccount] = useState(null);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">Web3 Voltorb Flip</h1>
-      <p className="mt-4 text-lg">The decentralized, high-stakes puzzle game!</p>
+    <main className="flex min-h-screen flex-col items-center  p-8 bg-gray-900 text-white">
+      <div className="w-full max-5-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="text-2xl font-bold">Web3 Voltorb Flip</p>
+          <WalletConnect
+            currentAccount={currentAccount}
+            setCurrentAccount={setCurrentAccount}
+          /> 
+      </div>
+
+      <div className="flex-grow flex flex-col items-center justify-center">
+        {/* This is where the main game board will go */}
+        {currentAccount ? (
+          <h2 className="text-xl mt-8">
+            Welcome, player!
+            Let&apos;s get ready to play.
+          </h2>
+        ) : (
+          <h2 className="text-xl mt-8">
+            Please Connect your wallet to start playing.
+          </h2>
+        )}
+      </div>
 
       {/* Game components will go here */}
     </main>
